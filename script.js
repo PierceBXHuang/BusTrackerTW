b40bd384f1c2a02122af27a353598718ec122768
PierceBXHuang <piercebxhuang.pt@gmail.com>
2026-07-27 19:49:27 +0800

    Initial homepage

---
+++ script.js
@@ -0,0 +1,23 @@
+const searchBtn=document.getElementById("searchBtn");
+
+const favoriteBtn=document.getElementById("favoriteBtn");
+
+const settingBtn=document.getElementById("settingBtn");
+
+searchBtn.onclick=function(){
+
+    alert("公車查詢功能即將完成！");
+
+}
+
+favoriteBtn.onclick=function(){
+
+    alert("收藏功能即將完成！");
+
+}
+
+settingBtn.onclick=function(){
+
+    alert("設定功能即將完成！");
+
+}
