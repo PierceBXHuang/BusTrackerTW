64f11b13378e3e5c5b8e1e617e8b21dd8f9c9d8a
PierceBXHuang <piercebxhuang.pt@gmail.com>
2026-08-06 23:06:31 +0800

    Revert "BusTracker v1.2 Stable"
    This reverts commit fa8c77cf82fefc9161b34bfe7f1bfb68e5b3ac7c.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bustracker-v2";
+const CACHE_NAME = "bus-tracker-v2";
 
 const urlsToCache = [
     "./",
@@ -6,25 +6,25 @@ const urlsToCache = [
     "./style.css",
     "./script.js",
     "./manifest.json",
-
-    "./icons/appbus-192.png",
-    "./icons/appbus-512.png"
+    "./icons/appbus.png"
 ];
 
+// 安裝
 self.addEventListener("install", event => {
+
+    self.skipWaiting();
 
     event.waitUntil(
 
         caches.open(CACHE_NAME)
 
-        .then(cache => cache.addAll(urlsToCache))
+            .then(cache => cache.addAll(urlsToCache))
 
     );
 
-    self.skipWaiting();
-
 });
 
+// 啟用
 self.addEventListener("activate", event => {
 
     event.waitUntil(
@@ -53,17 +53,26 @@ self.addEventListener("activate", event => {
 
 });
 
+// 請求
 self.addEventListener("fetch", event => {
 
     event.respondWith(
 
-        caches.match(event.request)
+        fetch(event.request)
 
-        .then(response => {
+            .then(response => {
 
-            return response || fetch(event.request);
+                const copy = response.clone();
 
-        })
+                caches.open(CACHE_NAME)
+
+                    .then(cache => cache.put(event.request, copy));
+
+                return response;
+
+            })
+
+            .catch(() => caches.match(event.request))
 
     );
 
